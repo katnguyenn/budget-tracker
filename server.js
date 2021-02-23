b039e518c577866f4347e5dc4077d17ed8a9a7d3
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-23 15:00:55 -0800

    deleted object from mongo connection

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect("mongodb://localhost/budget");
 
 // routes
 app.use(require("./routes/api.js"));

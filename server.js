81ebc848b42da86da5e0f8929e3a80ffb4dd45ca
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-02-26 23:14:17 -0800

    added changes to connect server discovery and monitoring engine

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget"), 
+{
+   useNewUrlParser: true,
+   useUnifiedTopology: true 
+};
 
 // routes
 app.use(require("./routes/api.js"));
